4850e9aa3cdb1071b0abf6d4a693061e56e36a99
SOHEELEE408 <dlwlsrl33@gmail.com>
2025-02-21 17:28:10 +0900

    컬럼 추가

--- localhost/study/member.sql
+++ localhost/study/member.sql
@@ -7,5 +7,6 @@ CREATE TABLE `member` (
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `money` int NOT NULL DEFAULT '0',
   `member_id` varchar(100) DEFAULT NULL,
+  `skeema_test_column` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
